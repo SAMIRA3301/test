6061c7a25b19709d1d3958c8fb45a7d7c8db3c73
agent-ilyusha <fingerstylena@gmail.com>
2023-10-03 20:43:40 +0300

    update main.py and test_degree.py

--- main.py
+++ main.py
@@ -2,25 +2,25 @@
 """Module for searching degree."""
 
 
-def search_degree(a: float, t: float, r: float, v: float):
-    """This function searches degree
+def search_degree(axiliration: float, time: float, radius: float, speed: float):
+    """This function searches degree.
 
     First, it is imported from ceil and pi from the math module,
      then the length of the circle is calculated,
      then the distance traveled is calculated,
      then the degree is returned
     Args:
-        t(float): time
-        a(float): acceleration
-        r(float): radius
-        v(float): speed
+        time(float): time
+        axiliration(float): acceleration
+        radius(float): radius
+        speed(float): speed
     Returns:
         float -> degree
 
     """
     from math import pi, ceil
-    length_circle = 2*pi*r
-    way = v * t + (a * t**2)/2
+    length_circle = 2*pi*radius
+    way = speed * time + (axiliration * time**2)/2
     factor = ceil(way/length_circle/360)
     return round(360*factor - way/length_circle, 2)
 
