fa89d4c189c2b75884d2fe8b3ba0006139cd5261
agent-ilyusha <fingerstylena@gmail.com>
2023-10-03 21:13:50 +0300

    update

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # --encoding: utf-8
-from math import pi, ceil
-
 """Module for searching degree."""
+
+from math import pi, ceil
 
 
 def search_degree(axiliration: float, time: float, radius: float, speed: float):
-    """Function for searches degree.
+    """Function is searching degree.
+
     First, it is imported from ceil and pi from the math module,
-     then the length of the circle is calculated,
-     then the distance traveled is calculated,
-     then the degree is returned
+    then the length of the circle is calculated,
+    then the distance traveled is calculated,
+    then the degree is returned
+
     Args:
         time(float)
         axiliration(float)
--- test_degree.py
+++ test_degree.py
@@ -2,6 +2,7 @@
 """Testing search degree."""
 
 import pytest
+
 from main import search_degree
 
 test = [
@@ -15,13 +16,17 @@ test = [
 
 @pytest.mark.parametrize('axiliration, time, radius, speed, exp', test)
 def test_degree(axiliration: float, time: float, radius: float, speed: float, exp: float):
-    """ Compares the resulting value with the response.
+    """Compares the resulting value with the response.
+
+    Function is comparing main answer and true answer
+
     Args:
-        axiliration: float
-        time: float
-        radius: float
-        speed: float
-        exp: float
+        axiliration(float)
+        time(float)
+        radius(float)
+        speed(float)
+        exp(float)
     :return:
+
     """
     assert search_degree(time, axiliration, radius, speed) == exp
